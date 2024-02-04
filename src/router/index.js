122ccb1f5883f3c27f968fa8ff28aa6ccefab68b
vasea <vasilii.costiuc@it-lab.md>
2024-02-04 22:08:55 +0200

    register form component added

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+ 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: import('../views/HomeView.vue'),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/Auth/RegisterView.vue"),
+    },
+  ],
+})
+ 
+export default router
